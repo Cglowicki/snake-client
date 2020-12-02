e39630454c364cd93aa552bf6d8672d96985d697
Cameron <camglowicki@hotmail.com>
2020-12-02 22:11:40 +0000

    setupInput and handleUser input for stdin and to handle termination of game

--- play.js
+++ play.js
@@ -1,3 +1,18 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function(data, key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
